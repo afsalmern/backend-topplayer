bb881632e21b53718ac17ca332b11748bda6af08
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-04-05 18:02:17 +0530

    changed token validation for dashboard routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -23,28 +23,27 @@ const uploadStt = multer({
     storage: multerStorageStt,
 });
 
-router.post('/category',[authMiddleware.checkAuthDasboard],  categoryController.addCategory);
-router.get('/category',[authMiddleware.checkAuthDasboard],  categoryController.getAllCategories);
-router.put('/category/:id',[authMiddleware.checkAuthDasboard],  categoryController.updateCategory);
-router.delete('/category/:id',[authMiddleware.checkAuthDasboard],  categoryController.deleteCategory);
+router.post('/category',[authMiddleware.checkUserAuth],  categoryController.addCategory);
+router.get('/category',[authMiddleware.checkUserAuth],  categoryController.getAllCategories);
+router.put('/category/:id',[authMiddleware.checkUserAuth],  categoryController.updateCategory);
+router.delete('/category/:id',[authMiddleware.checkUserAuth],  categoryController.deleteCategory);
 
-router.post('/course',[authMiddleware.checkAuthDasboard],  courseController.addCourse);
-router.get('/course',[authMiddleware.checkAuthDasboard],  courseController.getAllCourses);
-router.put('/course/:id',[authMiddleware.checkAuthDasboard],  courseController.updateCourse);
-router.delete('/course/:id',[authMiddleware.checkAuthDasboard],  courseController.deleteCourse);
+router.post('/course',[authMiddleware.checkUserAuth],  courseController.addCourse);
+router.get('/course',[authMiddleware.checkUserAuth],  courseController.getAllCourses);
+router.put('/course/:id',[authMiddleware.checkUserAuth],  courseController.updateCourse);
+router.delete('/course/:id',[authMiddleware.checkUserAuth],  courseController.deleteCourse);
 
-router.post('/subcourse',[authMiddleware.checkAuthDasboard],  subCourseController.addSubCourse);
-router.get('/subcourse',[authMiddleware.checkAuthDasboard],  subCourseController.getAllSubCourses);
-router.put('/subcourse/:id',[authMiddleware.checkAuthDasboard],  subCourseController.updateSubCourse);
-router.delete('/subcourse/:id',[authMiddleware.checkAuthDasboard],  subCourseController.deleteSubCourse);
+router.post('/subcourse',[authMiddleware.checkUserAuth],  subCourseController.addSubCourse);
+router.get('/subcourse',[authMiddleware.checkUserAuth],  subCourseController.getAllSubCourses);
+router.put('/subcourse/:id',[authMiddleware.checkUserAuth],  subCourseController.updateSubCourse);
+router.delete('/subcourse/:id',[authMiddleware.checkUserAuth],  subCourseController.deleteSubCourse);
 
+router.post('/video',[authMiddleware.checkUserAuth], uploadStt.single("video"),  videoUploadController.addVideo);
+router.get('/video',[authMiddleware.checkUserAuth], uploadStt.single("video"),  videoUploadController.getAllVideos);
+router.put('/video/:id',[authMiddleware.checkUserAuth], uploadStt.single("video"),  videoUploadController.updateVideo);
+router.delete('/video/:id',[authMiddleware.checkUserAuth], uploadStt.single("video"),  videoUploadController.deleteVideo);
 
-router.post('/video',[authMiddleware.checkAuthDasboard], uploadStt.single("video"),  videoUploadController.addVideo);
-router.get('/video',[authMiddleware.checkAuthDasboard], uploadStt.single("video"),  videoUploadController.getAllVideos);
-router.put('/video/:id',[authMiddleware.checkAuthDasboard], uploadStt.single("video"),  videoUploadController.updateVideo);
-router.delete('/video/:id',[authMiddleware.checkAuthDasboard], uploadStt.single("video"),  videoUploadController.deleteVideo);
-
-router.post('/uploadVideo',[authMiddleware.checkAuthDasboard], uploadStt.single("video") ,(req, res) =>{
+router.post('/uploadVideo',[authMiddleware.checkUserAuth], uploadStt.single("video") ,(req, res) =>{
   res.status(200).send({message:'saved successfully'})
 })
 
