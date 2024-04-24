3c3fd5ef12118e41fac72bea92c53b78ba33e2be
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-04-24 20:53:46 +0530

    added description field in course

--- models/course.js
+++ models/course.js
@@ -1,19 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
-    const Course = sequelize.define('courses', {
-        id: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        },
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        }, 
-        amount:{
-            type: Sequelize.FLOAT,
-        }
-    });
+  const Course = sequelize.define("courses", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: Sequelize.FLOAT,
+    },
+    description: {
+      type: Sequelize.TEXT, // Assuming description can be longer than a string
+      allowNull: true, // Depending on your requirements
+    },
+  });
 
-    return Course;
-}
+  return Course;
+};
