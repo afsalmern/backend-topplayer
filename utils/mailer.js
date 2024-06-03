1600e4fcf40ff55286f237bab06d089e2262e3be
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-06-03 17:44:51 +0530

    changed mail content

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,13 +1,5 @@
 const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   secure: true,
-//   auth: {
-//     user: process.env.EMAILID,
-//     pass: process.env.EMAILPASS,
-//   },
-// });
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
@@ -16,23 +8,16 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAILID,
     pass: process.env.EMAILPASS,
   },
-  // tls: {
-  //   // Do not fail on invalid certs
-  //   rejectUnauthorized: false,
-  //   minVersion: "TLSv1.2", // Ensure a minimum TLS version
-  // },
 });
-
 async function sendMail(to, subject, text, html) {
   try {
     // send mail with defined transport object
     const info = await transporter.sendMail({
-      from: "info.thetopplayer@gmail.com", // sender address
+      from: "noreply@thetopplayer.com", // sender address
       to: to, // list of receivers
       subject: subject, // Subject line
       text: text, // plain text body
       html: html, // html body
-      // cc: ["afsal@intersmart.in,salwa@intersmart.ae"],
     });
     console.log("Message sent: %s", info.messageId);
     return true; // Email sent successfully
