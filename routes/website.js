c0301a373b5631d7ec19ffeb7665c5f00ebc5dd6
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-06-13 15:55:56 +0530

    tamara integration

--- routes/website.js
+++ routes/website.js
@@ -3,11 +3,13 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth");
 const websiteController = require("../controller/website");
 const courseController = require("../controller/dashboard/course_controller");
-const {
-  getAllMainBanner,
-} = require("../controller/dashboard/main_banner_controller");
+const { getAllMainBanner } = require("../controller/dashboard/main_banner_controller");
+const { createTamaraPayment, tamaraWebHook } = require("../controller/tamarapay");
 
 const router = express.Router();
+
+router.post("/create-tamara-payment", createTamaraPayment);
+router.post("/tamara-webhook", tamaraWebHook);
 
 router.get("/courses", websiteController.getAllCourses);
 router.get("/courseById/:id", websiteController.getCourseById);
@@ -22,57 +24,24 @@ router.get("/banner", websiteController.getAllBanners);
 router.get("/main_banner", getAllMainBanner);
 router.get("/terms", websiteController.getTermsAndConditions);
 
-router.get(
-  "/subscribedCourse",
-  [authMiddleware.checkUserAuth],
-  websiteController.getSubscribedCourse
-);
-router.get(
-  "/course/:courseId",
-  [authMiddleware.checkUserAuth],
-  websiteController.getCourseMaterial
-);
-router.get(
-  "/getCourseById/:id",
-  [authMiddleware.checkUserAuth],
-  courseController.getCourseById
-);
-router.get(
-  "/subcourse/:courseId/:subCourseId",
-  [authMiddleware.checkUserAuth],
-  websiteController.getSubCourseMaterial
-);
+router.get("/subscribedCourse", [authMiddleware.checkUserAuth], websiteController.getSubscribedCourse);
+router.get("/course/:courseId", [authMiddleware.checkUserAuth], websiteController.getCourseMaterial);
+router.get("/getCourseById/:id", [authMiddleware.checkUserAuth], courseController.getCourseById);
+router.get("/subcourse/:courseId/:subCourseId", [authMiddleware.checkUserAuth], websiteController.getSubCourseMaterial);
 //router.post('/course',[authMiddleware.checkUserAuth],  websiteController.registerCourse);
 
-router.post(
-  "/video",
-  [authMiddleware.checkUserAuth],
-  websiteController.watchVideo
-);
+router.post("/video", [authMiddleware.checkUserAuth], websiteController.watchVideo);
 router.get("/video/:videoId/:courseId/:token", websiteController.getVideo);
-router.get(
-  "/videos/:courseId/:subCourseId/:day",
-  [authMiddleware.checkUserAuth],
-  websiteController.getVideos
-);
+router.get("/videos/:courseId/:subCourseId/:day", [authMiddleware.checkUserAuth], websiteController.getVideos);
 
-router.post(
-  "/create-payment-intent",
-  [authMiddleware.checkUserAuth],
-  websiteController.postStripePayment
-);
+router.post("/create-payment-intent", [authMiddleware.checkUserAuth], websiteController.postStripePayment);
 //router.post('/webhook',express.raw({type: 'application/json'}), websiteController.stripeWebhook);
 
 router.post("/subscribe", websiteController.subscribe);
 router.post("/contact", websiteController.contactUS);
 
-router.get(
-  "/payments",
-  [authMiddleware.checkUserAuth],
-  websiteController.payments
-);
+router.get("/payments", [authMiddleware.checkUserAuth], websiteController.payments);
 
 router.get("/visited", websiteController.getVisitors);
-
 
 module.exports = router;
