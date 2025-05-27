d7b00bd84d3ff4ca15360208887e8d6c7c70ba5e
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-05-27 15:05:56 +0530

    fix: import rateLimit for request limiting functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const rateLimit = require("express-rate-limit");
 const app = express();
 const helmet = require("helmet");
 const limiter = rateLimit({
